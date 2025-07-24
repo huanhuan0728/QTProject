ddb446073e61520a292f0d089211078325d7c740
卢绪焕 <1477405138@qq.com>
2025-07-24 13:52:26 +0800

    新提交

--- QSpliterProssss/mainwindow.cpp
+++ QSpliterProssss/mainwindow.cpp
@@ -11,7 +11,18 @@ MainWindow::MainWindow(QWidget *parent)
     QTextEdit *textEditMain = new QTextEdit("左边主窗口“， spMainWindow");
 
     QSplitter *spRight = new QSplitter(Qt::Vertical, spMainWindow);
-    QTextEdit *txtEditUp = new QTextEdit("右边上部分窗口“, spRight");
+    QTextEdit *txtEditUp = new QTextEdit("右边上部分窗口", spRight);
+    QTextEdit *txtEdiDown = new QTextEdit("右边下部分窗口", spRight);
+
+    QSplitter *sptest = new QSplitter(Qt::Vertical, spRight);
+    QTextEdit *txtEditTest = new QTextEdit("课程", sptest);
+
+    QSplitter *spTextEnd = new QSplitter(Qt::Horizontal, spMainWindow);
+    QTextEdit *txtEditTestEnd = new QTextEdit("lxh", spTextEnd);
+
+    spMainWindow->setWindowTitle("Splitter拆分窗口测试");
+    spMainWindow->show();
+
 }
 
 MainWindow::~MainWindow() {
